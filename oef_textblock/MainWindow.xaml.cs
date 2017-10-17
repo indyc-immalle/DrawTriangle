bce46cb0b0f8df2289e0eadaf404b4c5b448a3c2
Indy Caluwe <indy.caluwe@immalle.eu>
2017-10-17 09:47:23 +0200

    DrawTriangle  + TextBox

--- oef_textblock/MainWindow.xaml.cs
+++ oef_textblock/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace oef_textblock
+namespace oef_DrawTriangle
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -24,5 +24,40 @@ namespace oef_textblock
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush brush = new SolidColorBrush(Colors.Black);
+            int margin_y = Convert.ToInt32(TextBox_Y.Text);
+            int margin_x = Convert.ToInt32(TextBox_X.Text);
+
+            DrawTriangle(Canvas, brush, margin_x, margin_y, 20, 60);
+          
+
+        }
+        
+        private static void DrawTriangle(Canvas drawingArea, SolidColorBrush brushToUse, 
+                                         double xPlace, double yPlace, double width, double height)
+        {
+            DrawLine(drawingArea, brushToUse, xPlace, yPlace,
+                                              xPlace, yPlace + height);
+            DrawLine(drawingArea, brushToUse, xPlace, yPlace + height, 
+                                              xPlace + width, yPlace + height );
+            DrawLine(drawingArea, brushToUse, xPlace, yPlace,
+                                              xPlace + width, yPlace + height);
+        }
+
+        private static void DrawLine(Canvas drawingArea, SolidColorBrush brushToUse,  
+                                     double startX, double startY, double endX, double endY)
+        {
+            Line first = new Line();
+            first.X1 = startX;
+            first.Y1 = startY;
+            first.X2 = endX;
+            first.Y2 = endY;
+            first.Stroke = brushToUse;
+            drawingArea.Children.Add(first);
+        }
+
     }
 }
